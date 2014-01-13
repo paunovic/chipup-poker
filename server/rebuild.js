7b9dbb9598d35da87d6bebe0bf8b99f54a3bf5d0
michael bishop <cleverca22@gmail.com>
2014-01-13 18:11:42 -0400

    add chat events build codes.js from the proto

--- server/rebuild.js
+++ server/rebuild.js
@@ -1,5 +1,7 @@
 var fs = require('fs');
-var data = fs.readFileSync('client/src/uServerCodes.pas','utf8');
+var p = require("node-protobuf").Protobuf;
+
+/*var data = fs.readFileSync('client/src/uServerCodes.pas','utf8');
 var lines = data.split('\n');
 var out = fs.openSync("server/codes.js","w");
 fs.writeSync(out,'codes = {};\nmodule.exports = codes;\n');
@@ -12,5 +14,22 @@ for (var i=0; i<lines.length; i++) {
 		var name = res[1];
 		var id = parseInt(res[2]);
 		fs.writeSync(out,'codes.'+name+'='+id+';\n');
+	}
+}*/
+
+
+var pb = new p(fs.readFileSync("server/descriptor.desc"));
+var messages = fs.readFileSync("message.desc");
+var data = pb.Parse(messages,"google.protobuf.FileDescriptorSet");
+for (var i=0; i<data.file.length; i++) {
+	var file = data.file[i];
+	for (var j=0; j<file.enum_type.length; j++) {
+		var enum_type = file.enum_type[j];
+		var out = fs.openSync("server/"+enum_type.name+".js","w");
+		fs.writeSync(out,'codes = {};\nmodule.exports = codes;\n');
+		for (var x=0; x < enum_type.value.length; x++) {
+			var v = enum_type.value[x];
+			fs.writeSync(out,'codes.'+enum_type.value[x].name+'='+enum_type.value[x].number+';\n');
+		}
 	}
 }
