45050a53ae8be53f3f5e4031093614dc4b542287
michael bishop <cleverca22@gmail.com>
2014-01-15 17:30:58 -0400

    silly windows!

--- buildbot.bat
+++ buildbot.bat
@@ -1,4 +1,4 @@
 set PATH=cpp-generator/;%PATH%
-mkdir android/
-mkdir android/src/
+mkdir android\
+mkdir android\src
 protoc message.proto -o message.desc --java_out=android/src/ --delphi_out=delphi/
