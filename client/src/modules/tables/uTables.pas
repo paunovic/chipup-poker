e4441e37ea200185e61a14cc9d6c616f73dbf12a
Marko Paunovic <paunovic@gmail.com>
2014-01-17 10:20:43 +0100

    Fixed bug where same table could be opened multiple times

--- client/src/modules/tables/uTables.pas
+++ client/src/modules/tables/uTables.pas
@@ -12,18 +12,28 @@ type
     function AddTable(const AClub: TClubInfo; const AGame: TGameInfo): Boolean;
     procedure NotifyClose(const AGameId: TBytes);
     function SittingCount: Integer;
+    function IndexOf(const AGameId: TBytes): Integer;
+    function FindTable(const AGameId: TBytes; var ATable: TTable): Boolean;
   end;
 
 implementation
+
+uses
+  uCommon;
 
 
 function TTables.AddTable(const AClub: TClubInfo; const AGame: TGameInfo): Boolean;
 var
   table: TTable;
 begin
-  table := TTable.Create(self, AClub, AGame);
-  table.Form.Show;
-  Add(table);
+  if not FindTable(AGame.MongoId, table) then
+  begin
+    table := TTable.Create(self, AClub, AGame);
+    table.Form.Show;
+    Add(table);
+  end
+  else
+    table.Form.BringToFront;
   result := TRUE;
 end;
 
@@ -48,5 +58,29 @@ begin
     if ToArray[C1].IsSitting then
       Inc(result);
 end;
+
+function TTables.IndexOf(const AGameId: TBytes): Integer;
+var
+  C1      : Integer;
+  game_len: Integer;
+begin
+  game_len := Length(AGameId);
+  for C1 := 0 to Length(ToArray) - 1 do
+    if CompareBytes(AGameId, ToArray[C1].Game.MongoId, game_len) then
+      Exit(C1);
+  Exit(-1);
+end;
+
+function TTables.FindTable(const AGameId: TBytes; var ATable: TTable): Boolean;
+var
+  index: Integer;
+begin
+  index := IndexOf(AGameId);
+  if index = -1 then
+    Exit(FALSE);
+  ATable := ToArray[index];
+  Exit(TRUE);
+end;
+
 
 end.
