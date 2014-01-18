dd27e54a39367c1c945160b83e2a965ccae44e1a
michael bishop <clever@nbnet.nb.ca>
2014-01-18 16:39:19 -0400

    last change!!

--- buildproto.bat
+++ buildproto.bat
@@ -1,4 +1,4 @@
 set PATH=cpp-protobuf-generator/;%PATH%
-protoc message.proto --delphi_out=output/
+protoc message.proto --delphi_out=cpp-protobuf-generator/output/
 copy /y "cpp-protobuf-generator\output\uServerCodes.pas" "client\src\modules\protobuf\objects\uServerCodes.pas"
 
