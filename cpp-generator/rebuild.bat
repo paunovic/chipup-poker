7bd6ba1883118587aabd89f1987a0680c34a3130
Marko Paunovic <paunovic@gmail.com>
2014-01-18 20:50:20 +0100

    Signed-off-by: Marko Paunovic <paunovic@gmail.com>

--- cpp-generator/rebuild.bat
+++ cpp-generator/rebuild.bat
@@ -1,3 +1,5 @@
 cd ..
 set PATH=cpp-generator/;%PATH%
 protoc message.proto --delphi_out=cpp-generator/output
+copy /y "cpp-generator\output\uServerCodes.pas" "client\src\uServerCodes.pas"
+cd cpp-generator
