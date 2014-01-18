92df96e3da291a495e93115d05f7bdafa6704c6f
michael bishop <cleverca22@gmail.com>
2014-01-18 15:36:29 -0400

    fix a path

--- buildbot.bat
+++ buildbot.bat
@@ -1,4 +1,4 @@
-set PATH=cpp-generator/;%PATH%
+set PATH=cpp-protobuf-generator/;%PATH%
 mkdir android\
 mkdir android\src
 protoc message.proto -o message.desc --java_out=android/src/ --delphi_out=delphi/
