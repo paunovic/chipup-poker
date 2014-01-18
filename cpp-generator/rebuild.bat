ea7dcf522592baee58566ccdd62440231298039d
Marko Paunovic <paunovic@gmail.com>
2014-01-18 21:16:52 +0100

    remove the file

--- cpp-generator/rebuild.bat
+++
@@ -1,5 +0,0 @@
-cd ..
-set PATH=cpp-generator/;%PATH%
-protoc message.proto --delphi_out=cpp-generator/output
-copy /y "cpp-generator\output\uServerCodes.pas" "client\src\uServerCodes.pas"
-cd cpp-generator
