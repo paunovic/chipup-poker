601f7a12f31297a8fb3a263279153bc6baa0a0a3
Marko Paunovic <paunovic@gmail.com>
2014-01-24 04:09:58 +0100

    Signed-off-by: Marko Paunovic <paunovic@gmail.com>

--- client/src/modules/uSocketClient.pas
+++ client/src/modules/uSocketClient.pas
@@ -420,8 +420,11 @@ begin
     seChat: ADataObject := TPB_ChatEvent.Create(ADataPointer, ARpcMessage.DataSize);
     seSecondaryLoginDetected: ;
     seAccountConfirmed: ADataObject := TPB_TableStatus.Create(ADataPointer, ARpcMessage.DataSize);
-    seClubChange: ADataObject := TPB_Club.Create(ADataPointer, ARpcMessage.DataSize);
+    seClubChange,
     seClubDeleted: ADataObject := TPB_Club.Create(ADataPointer, ARpcMessage.DataSize);
+    seGameChange,
+    seGameCreate,
+    seGameDelete: ADataObject := TPB_Game.Create(ADataPointer, ARpcMessage.DataSize);
   else
     result := FALSE;
     {$IFDEF DEBUG} DebugLn(Format('Unhandled MethodId received: %d', [ARpcMessage.MethodId]), ditException); {$ENDIF}
