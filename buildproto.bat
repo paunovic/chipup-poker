8fa6d7c02a4eb1ff1a11ecf53a2d0faae9a09256
michael bishop <cleverca22@gmail.com>
2014-02-25 15:43:06 -0400

    fix buildproto to include common some updates to the testclient

--- buildproto.bat
+++ buildproto.bat
@@ -2,5 +2,5 @@ set PATH=C:/dev-cpp/bin/;cpp-protobuf-generator/;%PATH%
 cd cpp-protobuf-generator
 make
 cd ..
-protoc message.proto -o message.desc --delphi_out=cpp-protobuf-generator/output/
+protoc message.proto common.proto -o message.desc --delphi_out=cpp-protobuf-generator/output/
 copy /y "cpp-protobuf-generator\output\uServerCodes.pas" "client\src\modules\protobuf\objects\uServerCodes.pas"
