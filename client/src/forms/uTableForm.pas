775be42f430deed20ed3778a58a246b106cdf167
michael bishop <cleverca22@gmail.com>
2014-03-03 17:49:23 -0400

    profile the redraw time

--- client/src/forms/uTableForm.pas
+++ client/src/forms/uTableForm.pas
@@ -292,7 +292,9 @@ var
   chat_width: Integer;
   tblx, tbly: Integer;
   tblw, tblh: Integer;
+  start,stop: Integer;
 begin
+  start := GetTickCount;
   paBottom.Height := Round(Height / 5);
   chat_width := Round(Width / 2.5);
 
@@ -360,8 +362,10 @@ begin
   ShowDebugInfo;
   {$ENDIF}
 
+  stop := GetTickCount;
   if APaintboxRepaint then
     PaintBox.Flush;
+  DebugLn(Format('redraw time: %d', [stop - start]), ditApplication);
 end;
 
 procedure TfrmTable.seRaiseAmountPropertiesChange(Sender: TObject);
