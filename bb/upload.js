c42df1cc52e0e1eebc114f678cc55cc9fac2d53a
michael bishop <cleverca22@gmail.com>
2014-03-21 14:03:42 -0300

    adding the uploader

---
+++ bb/upload.js
@@ -0,0 +1,45 @@
+var fs = require('fs');
+var child = require('child_process');
+var http = require('http');
+
+var data = fs.readFileSync('../client/src/modules/settings/Poker.HardcodedSettings.pas');
+var res = /VERSION *: *'([0-9.a-z]+)';/.exec(data);
+console.log('version='+res[1]);
+
+child.exec('git rev-parse HEAD',function (err,stdout,stderr) {
+	console.log('revision='+stdout.trim());
+	fs.writeFileSync("version.inc",'revision='+stdout.trim());
+	finish(stdout.trim());
+});
+
+function finish(version) {
+	var key = 'abcd';
+	var header = '--'+key+'\r\n'+
+		'Content-Type: application/octed-stream\r\n'+
+		'Content-Disposition: form-data; name="installer"; filename="chipup_poker_install.exe"\r\n'+
+		'Content-Transfer-Encoding: binary\r\n\r\n';
+	var footer = '\r\n--'+key+'--';
+
+	var textsize = header.length + footer.length;
+	var filesize = fs.statSync('../client/installer/chipup_poker_install.exe').size;
+
+	var request = http.request({host:'chipuppoker.com',method:'POST',path:'/newVersion?version='+res[1]+'&revision='+version+
+		'&debug='+process.argv[2],
+		headers:{'Content-Length':textsize+filesize}
+	},function (res) {
+		console.log('done?');
+		res.on('data',function (chunk) {
+			console.log('body:'+chunk);
+		});
+	});
+	console.log('doing post');
+	request.setHeader('Content-Type','multipart/form-data; boundary="'+key+'"');
+	request.write(header);
+	console.log('making stream');
+	fs.createReadStream('../client/installer/chipup_poker_install.exe',{bufferSize: 4*1024})
+		.on('end',function () {
+			console.log('ending');
+			request.end(footer);
+		})
+		.pipe(request,{end:false});
+}
