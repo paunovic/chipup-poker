0c4785adb2512249a7643b268f206b211721badd
Marko Paunovic <paunovic@gmail.com>
2014-03-22 22:46:41 +0100

    Signed-off-by: Marko Paunovic <paunovic@gmail.com>

--- client/src/modules/settings/Poker.HardcodedSettings.pas
+++ client/src/modules/settings/Poker.HardcodedSettings.pas
@@ -33,7 +33,7 @@ type
     const
       Hardcoded: THardcodedSettingsRec = (
                                            // version of app
-                                           VERSION: '0.01a.0015';
+                                           VERSION: '0.01a.0016';
                                            REVISION: {$I revision.inc};
 
                                            // instance mutex name
