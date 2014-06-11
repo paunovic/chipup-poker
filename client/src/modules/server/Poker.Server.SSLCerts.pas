48a8602230089e82ff29d576695d614db71f8949
michael bishop <cleverca22@gmail.com>
2014-06-11 16:40:40 -0300

    made note of a possible fix for an error

--- client/src/modules/server/Poker.Server.SSLCerts.pas
+++ client/src/modules/server/Poker.Server.SSLCerts.pas
@@ -26,6 +26,7 @@ var
   bio: PBio;
   rstream: TResourceStream;
 begin
+  // FIXME, catch the error about libeay32.dll being missing and handle it better
   InitializeSsl;
 
   rstream := TResourceStream.Create(HInstance, AResourceName, RT_RCDATA);
