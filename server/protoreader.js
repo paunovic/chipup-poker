a9b3dc0d4c822ba93c8055b26b99b93c2ca333fc
Pavle Lekic <lekic.pavle@yahoo.com>
2014-07-03 13:25:01 +0200

    Revert "refactored buffer not to use this + removed comments"
    This reverts commit ce40c0dd724f47c58f45ae8bd1669914d0ae65a0.

--- server/protoreader.js
+++ server/protoreader.js
@@ -1,53 +1,58 @@
-module.exports = protoreader;
-
 var util = require('util');
-var pb,codes,hidden, buffer;
 
+module.exports = protoreader;
+var pb,codes,hidden;
 function protoreader(socket,handler,error) {
 	if (!(this instanceof protoreader)) return new protoreader(socket,handler,error);
 	this.socket = socket;
 	this.handler = handler;
-	buffer = null;
+	this.buffer = null;
 	this.error = error;
 	function process_packet() {
-		if (buffer.length < 2) {
+		if (this.buffer.length < 2) {
 			console.log('size prefix not in buffer yet');
 			return;
 		}
-		var headersize = buffer.readInt16LE(0);
-		if (buffer.length < (2 + headersize)) {
-			console.log('header not in buffer yet', headersize);
+		var headersize = this.buffer.readInt16LE(0);
+		if (this.buffer.length < (2+headersize)) {
+			console.log('header not in buffer yet',headersize);
 			return false;
 		}
+		//console.log('\nheader size:',headersize,this.buffer);
 		if (headersize > 0) {
-			var header = buffer.slice(2, 2 + headersize);
+			var header = this.buffer.slice(2,2+headersize);
 			try {
 				header = pb.Parse(header,'Poker.RpcMessage');
 				if (!header.DataSize) header.DataSize = 0;
+				//console.log('header is',header);
 			} catch (e) {
 				console.log(header);
 				this.error(e);
 				return false;
 			}
-			if (buffer.length < (2 + headersize + header.DataSize)) {
+			if (this.buffer.length < (2+headersize+header.DataSize)) {
 				console.log('arguments not in buffer yet');
 				return false;
 			}
-			var args = buffer.slice(2 + headersize, 2 + headersize + header.DataSize);
+			var args = this.buffer.slice(2+headersize,2+headersize+header.DataSize);
+		//try {
 			this.handler(header.MethodId,args);
-			buffer = buffer.slice(2 + headersize + header.DataSize);
+		//} catch (e) {
+		//	this.handler.error(e);
+		//}
+			this.buffer = this.buffer.slice(2+headersize+header.DataSize);
 		} else {
-			buffer = buffer.slice(2);
+			this.buffer = this.buffer.slice(2);
 		}
 		return true;
 	}
-
 	this.socket.on('data',function (chunk) {
-		if (buffer) {
-			buffer = Buffer.concat([buffer, chunk]);
+		if (this.buffer) {
+			this.buffer = Buffer.concat([this.buffer,chunk]);
 		}
-		else buffer = chunk;
-		while ((buffer.length > 0) && process_packet.call(this)) { }
+		else this.buffer = chunk;
+		while ((this.buffer.length > 0) && process_packet.call(this)) { }
+		//if (this.buffer.length > 0) this.handler.log('remaining data:',this.buffer);
 	}.bind(this));
 }
 protoreader.init = function init(input,mapping,hiddenin) {
@@ -59,7 +64,6 @@ protoreader.reply = function reply(code,message,type) {
 	code = parseInt(code);
 	var args;
 	var datasize = 0;
-
 	if (type == 'raw') {
 		args = message;
 		datasize = args.length;
@@ -67,29 +71,26 @@ protoreader.reply = function reply(code,message,type) {
 		args = pb.Serialize(message,type);
 		datasize = args.length;
 	}
-
 	var object = { MethodId:code, DataSize:datasize };
 	var header = pb.Serialize(object, "Poker.RpcMessage");
 	var totalsize = 2 + header.length + datasize;
 	var messageOut = new Buffer(totalsize);
-
 	messageOut.writeUInt16LE(header.length,0);
 	header.copy(messageOut,2);
-
 	if (datasize > 0) args.copy(messageOut,2+header.length);
-
 	var alldone = this.socket.write(messageOut);
-
 	if (!alldone) {
 		if (this.socket._handle) this.log('partial message write %d %s',this.socket.bufferSize,util.inspect({a:this.socket._handle.writeQueueSize,b:this.socket._writableState.length}));
 	}
 	if (this.socket._writableState.length > (256 * 1024)) {
 		this.error('sendq overflow');
 	}
-
+	//console.log('header out:',header);
+	//console.log(object);
 	if ([codes.PerClientMsgEvent,codes.seChat,codes.scTableSit,codes.scTableJoin,codes.scLogin,codes.scStatus,codes.seGameChange,codes.PerGameMsgEvent].indexOf(code) != -1) {
 	} else if (hidden && hidden.indexOf(code) != -1) {
 	} else if (code == 100) this.log('sent '+datasize+' bytes for code '+code,message);
 	else this.log('sent %d bytes for code %s',datasize,codes.reverse[code]);
+	//console.log(message);
 }
 
