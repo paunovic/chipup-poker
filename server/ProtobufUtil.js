b9b3f56c85c2ac21b2e183f69e2b9527fd02fa15
Pavle Lekic <lekic.pavle@yahoo.com>
2014-07-07 19:17:46 +0200

    ProtobufUtil class, does same things as protoreader, but is cleaner

---
+++ server/ProtobufUtil.js
@@ -0,0 +1,77 @@
+'use strict';
+
+module.exports = ProtobufUtil;
+
+function ProtobufUtil(protobuf, schema) {
+	this.protobuf = protobuf;
+	this.schema = schema;
+}
+
+ProtobufUtil.prototype.createOnDataListenerFn = function (callback, logger) {
+	logger = typeof logger !== 'undefined' ?  logger : console.log;
+	var buffer = new Buffer(0);
+
+	return function (chunk) {
+		buffer = Buffer.concat([buffer, chunk]);
+
+		while (buffer.length >= 2) {
+			var headerSize = buffer.readInt16LE(0);
+
+			if (headerSize === 0) {
+				buffer = buffer.slice(2);
+				continue;
+			}
+
+			if (buffer.length < 2 + headerSize) {
+				logger('header not in buffer yet');
+				break;
+			}
+
+			var headerUnparsed = buffer.slice(2, 2 + headerSize);
+
+			try {
+				var header = this.protobuf.Parse(headerUnparsed, this.schema);
+
+				if (!header.DataSize)
+					header.DataSize = 0;
+
+				if (buffer.length < (2 + headerSize + header.DataSize)) {
+					logger('arguments not in buffer yet');
+					break;
+				}
+
+				var args = buffer.slice(2 + headerSize, 2 + headerSize + header.DataSize);
+				callback(null, header.MethodId, args);
+				buffer = buffer.slice(2 + headerSize + header.DataSize);
+			} catch (e) {
+				callback(e);
+			}
+		}
+	};
+};
+
+
+ProtobufUtil.prototype.encode = function (code, message, type) {
+	code = parseInt(code);
+	var args, dataSize = 0;
+
+	if (message) {
+		args = type === 'raw' ? message : this.protobuf.Serialize(message, type);
+		dataSize = args.length;
+	}
+
+	var object = {
+		MethodId: code,
+		DataSize: dataSize
+	};
+	var header = this.protobuf.Serialize(object, this.schema);
+	var encodedMessage = new Buffer(2 + header.length + dataSize);
+	encodedMessage.writeUInt16LE(header.length, 0);
+	header.copy(encodedMessage, 2);
+
+	if (dataSize > 0)
+		args.copy(encodedMessage, 2 + header.length);
+
+	return encodedMessage;
+};
+
