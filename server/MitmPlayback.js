7a9d3e59a44cee7ded30ad19c2e0f280eb413a13
michael bishop <cleverca22@gmail.com>
2014-07-16 16:10:55 -0300

    fix a typo

--- server/MitmPlayback.js
+++ server/MitmPlayback.js
@@ -140,7 +140,7 @@ MitmPlayback.prototype._checkIfSingleRequestMatch = function (methodId, args, ty
 		var argsParsed = this._makeArgsAndSanatize(args, methodId, argsFromDb, requestFromDb);
 		var difference = diff(argsParsed.fromServer, argsParsed.fromDb);
 		console.log("A-server, B-from db\n%j\n%j\ndiff:%j\n", argsParsed.fromServer, argsParsed.fromDb, difference);
-		if (!difference) throw new Error('Requests do not match! ' + currentRequestInfo + ' ' + methodName + ' vs ' + requestFromDb.method);
+		if (difference) throw new Error('Requests do not match! ' + currentRequestInfo + ' ' + methodName + ' vs ' + requestFromDb.method);
 	}
 
 	if (type !== requestFromDb.type)
