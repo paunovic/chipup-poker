f1259654405fac59b756cb45c7b238ba7f2bbbd6
michael bishop <cleverca22@gmail.com>
2014-07-16 15:59:38 -0300

    label the output some

--- server/MitmPlayback.js
+++ server/MitmPlayback.js
@@ -139,7 +139,7 @@ MitmPlayback.prototype._checkIfSingleRequestMatch = function (methodId, args, ty
 
 		var argsParsed = this._makeArgsAndSanatize(args, methodId, argsFromDb, requestFromDb);
 		var difference = diff(argsParsed.fromServer, argsParsed.fromDb);
-		console.log("A-server, B-from db\n%j\n%j\n%j\n", argsParsed.fromServer, argsParsed.fromDb, difference);
+		console.log("A-server, B-from db\n%j\n%j\ndiff:%j\n", argsParsed.fromServer, argsParsed.fromDb, difference);
 		throw new Error('Requests do not match! ' + currentRequestInfo + ' ' + methodName + ' vs ' + requestFromDb.method);
 	}
 
