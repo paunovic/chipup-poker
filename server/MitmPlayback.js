b012bcf8ad913711fb7a023d0ee065f2cd9856e5
Pavle Lekic <lekic.pavle@yahoo.com>
2014-07-16 23:40:28 +0200

    added seGameChange to ignore list

--- server/MitmPlayback.js
+++ server/MitmPlayback.js
@@ -21,7 +21,7 @@ function MitmPlayback(config) {
 };
 
 
-MitmPlayback.prototype.IGNORE_METHODS = ["scPing", "srPong", "srTableStatsReply"];
+MitmPlayback.prototype.IGNORE_METHODS = ["scPing", "srPong", "srTableStatsReply", "seGameChange"];
 
 
 MitmPlayback.prototype.startPlayback = function() {
