959cc994178f34bdd1a3e02efdca2fd97e1212c5
michael bishop <cleverca22@gmail.com>
2014-07-23 20:51:15 -0300

    attempt #1 at fixing upload

--- bb/upload.js
+++ bb/upload.js
@@ -30,7 +30,8 @@ function doUpload(version,host) {
 
 	var request = https.request({hostname:host,method:'POST',path:'/newVersion?version='+res[1]+'&revision='+version+
 		'&debug='+process.argv[2],
-		headers:{'Content-Length':textsize+filesize1}
+		headers:{'Content-Length':textsize+filesize1},
+		ca:fs.readFileSync('sub.class1.server.ca.pem')
 	},function (res) {
 		console.log('%s reply',host);
 		res.on('data',function (chunk) {
