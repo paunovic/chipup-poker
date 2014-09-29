0682126c86f8842bfa4db6d542c17861f40de896
Marko Paunovic <paunovic@gmail.com>
2014-09-29 14:44:21 +0700

    Signed-off-by: Marko Paunovic <paunovic@gmail.com>

--- buildproto-vs.bat
+++ buildproto-vs.bat
@@ -1,7 +1,7 @@
 set PATH=cpp-protobuf-generator\;cpp-protobuf-generator\cpp-protobuf-generator\debug\;%PATH%
 cd cpp-protobuf-generator
 mkdir output
-"c:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" "cpp-protobuf-generator\cpp-protobuf-generator-vs.sln"
+"c:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" "cpp-protobuf-generator\cpp-protobuf-generator.sln"
 cd ..
 protoc message.proto backend.proto common.proto -o message.desc --delphi_out=cpp-protobuf-generator/output/
 protocopier\bin\Win32\release\protocopier.exe
