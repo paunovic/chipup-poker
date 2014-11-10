aae869eb18404ed7000ebb3ea3db218381f27a30
michael bishop <cleverca22@gmail.com>
2014-11-10 02:35:27 -0600

    change to https and fix cookie failure

--- server/buildbot.js
+++ server/buildbot.js
@@ -1,4 +1,4 @@
-var http = require('http');
+var https = require('http');
 var globalCookies = {};
 var config = require('./config');
 module.exports.forceBuild = forceBuild;
@@ -19,7 +19,7 @@ function forceBuild(builder,rev) {
 	for (var key in globalCookies) {
 		headers.Cookie.push(key+'='+globalCookies[key]);
 	}
-	var req = http.request({host:'buildbot.chipuppoker.com',method:'POST',path:'/builders/'+builder+'/force',headers:headers,auth:'node:Eiwae5ah'},function (reply) {
+	var req = https.request({host:'buildbot.chipuppoker.com',method:'POST',path:'/builders/'+builder+'/force',headers:headers,auth:'node:Eiwae5ah'},function (reply) {
 		console.log(reply.headers);
 		console.log(reply.statusCode);
 		req.on('data',function (chunk) {
@@ -34,7 +34,7 @@ function forceBuild(builder,rev) {
 }
 function doLogin(cb) {
 	var body = new Buffer('username=node&passwd='+config.bbpassword);
-	var req = http.request({host:'buildbot.chipuppoker.com',method:'POST',path:'/login',headers:{'Content-Length':body.length,'Content-Type':'application/x-www-form-urlencoded'},auth:'node:Eiwae5ah'},function (reply) {
+	var req = https.request({host:'buildbot.chipuppoker.com',method:'POST',path:'/login',headers:{'Content-Length':body.length,'Content-Type':'application/x-www-form-urlencoded'},auth:'node:Eiwae5ah'},function (reply) {
 		console.log(reply.headers);
 		console.log(reply.statusCode);
 		req.on('data',function (chunk) {
@@ -44,7 +44,7 @@ function doLogin(cb) {
 			console.log('http error logging into buildbot:',err);
 		});
 		var cookies = reply.headers['set-cookie'];
-		if (!cookies) return;
+		if (!cookies) return cb();
 		for (var x=0; x<cookies.length; x++) {
 			var c = cookies[x].split(';')[0].split('=');
 			globalCookies[c[0]] = c[1];
