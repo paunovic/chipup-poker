739e8d54310575c50effc98323aa34c3b56f6091
michael bishop <cleverca22@gmail.com>
2015-03-22 00:21:02 -0300

    oops, missed a line

--- qt-client/client/table.cpp
+++ qt-client/client/table.cpp
@@ -128,6 +128,7 @@ void Table::setGame(const Data::Game *game, const Data::Club *club) {
 	p->setGame(game);
 	setWindowTitle(QString(tr("%1 (%2/%3 %4) - %5")).arg(game->gamename).arg(game->sb).arg(game->bb).arg(game->typeToString()).arg(club->name));
 #if 1
+	QFile input("E:\\poker\\poker\\qt-client\\client\\table.js")
 	p->loadJs(jscode,"table.js");
 #else
 	p->loadJsFromResource();
