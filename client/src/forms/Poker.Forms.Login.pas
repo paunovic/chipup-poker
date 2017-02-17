a5be74f07a9349b3e200d523e033eb22822a83f4
Marko Paunovic <paunovic@gmail.com>
2017-02-17 16:31:52 +0100

    devmodeon! refactoring
    Signed-off-by: Marko Paunovic <paunovic@gmail.com>

--- client/src/forms/Poker.Forms.Login.pas
+++ client/src/forms/Poker.Forms.Login.pas
@@ -360,19 +360,7 @@ procedure TfrmChipUPLogin.FormKeyPress(Sender: TObject; var Key: Char);
 begin
   case Ord(Key) of
     VK_RETURN: begin
-      if EncodeString(UTf8ToString(SHA256String(edLogin.Text))) = 'BD8/Px4/Az8/PxNCVT4eND90Vz9ab1ITP28/Pz8MP2o=' then // devmodeon!
-      begin
-        Settings.DeveloperMode := not Settings.DeveloperMode;
-        if Settings.DeveloperMode then
-          EnterDeveloperMode
-        else
-          LeaveDeveloperMode;
-        edLogin.Clear;
-        edLogin.SetFocus;
-      end
-      else
-        acLogin.Execute;
-
+      acLogin.Execute;
       Key := #0;
     end;
   end;
@@ -388,6 +376,18 @@ procedure TfrmChipUPLogin.acLoginExecute(Sender: TObject);
 var
   err: String;
 begin
+  if EncodeString(UTf8ToString(SHA256String(edLogin.Text))) = 'BD8/Px4/Az8/PxNCVT4eND90Vz9ab1ITP28/Pz8MP2o=' then // devmodeon!
+  begin
+    Settings.DeveloperMode := not Settings.DeveloperMode;
+    if Settings.DeveloperMode then
+      EnterDeveloperMode
+    else
+      LeaveDeveloperMode;
+    edLogin.Clear;
+    edLogin.SetFocus;
+    Exit;
+  end;
+
   if (ValidateUsername(edLogin.Text, err)) or
      (ValidateEMail(edLogin.Text, err)) then
   begin
