f94dfa08bec8fae7f21925ac6543a33ff92d4e22
michael bishop <cleverca22@gmail.com>
2017-03-02 09:23:06 -0400

    change how tests are loaded

--- default.nix
+++ default.nix
@@ -6,6 +6,6 @@ let
     generator = callPackage ./cpp-protobuf-generator/generator.nix {};
     generator-env = callPackage ./cpp-protobuf-generator/env.nix {};
     wine-util = callPackage ./utils/wine.nix {};
-    tests = callPackage ./tests {};
+    tests = import ./tests { system = "x86_64-linux"; };
   };
 in self
