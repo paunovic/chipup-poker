6d38a7d72096f778c83b7a6e4e47a75487897020
michael bishop <cleverca22@gmail.com>
2017-03-02 11:44:25 +0000

    add a buildbot patch

--- nixos/config.nix
+++ nixos/config.nix
@@ -2,5 +2,8 @@
   packageOverrides = pkgs: rec {
     poker = (pkgs.callPackage ../server {}).package;
     toxvpn = pkgs.callPackage ./toxvpn.nix {};
+    buildbot = pkgs.buildbot.overrideDerivation (old: {
+      patches = [ ./buildbot.patch ];
+    });
   };
 }
