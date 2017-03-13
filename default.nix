c1ce58f5fc4a28082711b8407ca1e0f0e3dab142
michael bishop <cleverca22@gmail.com>
2017-03-13 17:02:12 -0300

    add an override to smooth out the differences between channels

--- default.nix
+++ default.nix
@@ -1,8 +1,12 @@
 let
-  pkgs = import <nixpkgs> {};
+  config = {
+    packageOverrides = pkgs_in: {
+      runCommandCC = if pkgs_in ? runCommandCC then pkgs_in.runCommandCC else pkgs_in.runCommand;
+    };
+  };
+  pkgs = import <nixpkgs> { inherit config; };
   callPackage = pkgs.newScope self;
-  runCommandCC = if pkgs ? runCommandCC then pkgs.runCommandCC else pkgs.runCommand;
-  multiArchSet = system: let arch_pkgs = import <nixpkgs> { inherit system; }; in rec {
+  multiArchSet = system: let arch_pkgs = import <nixpkgs> { inherit system config; }; in rec {
     client = arch_pkgs.qt5.callPackage ./qt-client/client.nix { inherit protos; };
     protos = arch_pkgs.callPackage ./protos { inherit (arch_pkgs) runCommandCC; };
   };
@@ -14,7 +18,7 @@ let
     wine-util = callPackage ./utils/wine.nix {};
     tests = import ./tests { system = "x86_64-linux"; };
     test-driver = callPackage ./tests/test-driver.nix {};
-    protos = callPackage ./protos { inherit runCommandCC; };
+    protos = callPackage ./protos { };
     "x86_64-darwin" = multiArchSet "x86_64-darwin";
   };
 in self
